54aa152423391067d576dd60c26dc56465afc892
Daniel Rivers <me@danielrivers.com>
2024-02-20 12:53:15 +0000

    fix: not adding auth params

--- src/runtime/server/api/login.get.ts
+++ src/runtime/server/api/login.get.ts
@@ -2,11 +2,12 @@ import { defineEventHandler, sendRedirect } from 'h3'
 import { getKindeClient } from '../utils/client'
 
 export default defineEventHandler(async event => {
-  const {org_code, authUrlParams, postLoginRedirectURL} = getQuery(event)
-
-  const loginURL = await getKindeClient().login(event.context.kinde.sessionManager, {
+  const query = getQuery(event)
+  const {org_code, postLoginRedirectURL} = query
+  const loginURL = await getKindeClient().login(event.context.kinde.sessionManager,
+  {
     org_code: org_code?.toString(),
-    authUrlParams: authUrlParams ? JSON.parse(authUrlParams.toString()) : null,
+    authUrlParams: query,
     post_login_redirect_url: postLoginRedirectURL?.toString()
   })
   await sendRedirect(event, loginURL.href)
