2c2a5632a5984d9c65712bc4567c7d6fb1ac5b44
Daniel Roe <daniel@roe.dev>
2024-03-05 13:17:24 +0000

    style: lint

--- src/runtime/server/api/register.get.ts
+++ src/runtime/server/api/register.get.ts
@@ -2,9 +2,6 @@ import { defineEventHandler, sendRedirect } from 'h3'
 import { getKindeClient } from '../utils/client'
 
 export default defineEventHandler(async event => {
-  const registerURL = await getKindeClient().register(
-    event.context.kinde.sessionManager,
-    getQuery(event)
-  )
+  const registerURL = await getKindeClient().register(event.context.kinde.sessionManager, getQuery(event))
   await sendRedirect(event, registerURL.href)
 })
