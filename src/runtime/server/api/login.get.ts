efd641a7482c31eb4912fa33e648f0caf7c15e89
Daniel Roe <daniel@roe.dev>
2024-03-13 15:01:11 -0700

    style: lint

--- src/runtime/server/api/login.get.ts
+++ src/runtime/server/api/login.get.ts
@@ -3,7 +3,12 @@ import { getKindeClient } from '../utils/client'
 
 export default defineEventHandler(async event => {
   const { kinde: kindeSettings } = useRuntimeConfig()
-  const query: Record<string, string> = getQuery(event);
-  const loginURL = await getKindeClient().login(event.context.kinde.sessionManager, { authUrlParams: { audience: kindeSettings.audience, ...query }}, )
+  const query: Record<string, string> = getQuery(event)
+  const loginURL = await getKindeClient().login(event.context.kinde.sessionManager, {
+    authUrlParams: {
+      audience: kindeSettings.audience,
+      ...query
+    }
+  })
   await sendRedirect(event, loginURL.href)
 })
--- src/runtime/server/api/register.get.ts
+++ src/runtime/server/api/register.get.ts
@@ -3,7 +3,12 @@ import { getKindeClient } from '../utils/client'
 
 export default defineEventHandler(async event => {
   const { kinde: kindeSettings } = useRuntimeConfig()
-  const query: Record<string, string> = getQuery(event);
-  const registerURL = await getKindeClient().register(event.context.kinde.sessionManager, { authUrlParams: { audience: kindeSettings.audience, ...query} })
+  const query: Record<string, string> = getQuery(event)
+  const registerURL = await getKindeClient().register(event.context.kinde.sessionManager, {
+    authUrlParams: {
+      audience: kindeSettings.audience,
+      ...query
+    }
+  })
   await sendRedirect(event, registerURL.href)
 })
