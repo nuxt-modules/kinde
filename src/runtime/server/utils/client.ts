b65bcfc040e9443e3a920afd7d7f3bc69e8f7a5a
Daniel Roe <daniel@roe.dev>
2024-04-10 20:49:41 +0200

    chore: move comment

--- src/runtime/server/utils/client.ts
+++ src/runtime/server/utils/client.ts
@@ -1,9 +1,9 @@
 import { createKindeServerClient, GrantType } from '@kinde-oss/kinde-typescript-sdk'
 import type { ACClient } from '@kinde-oss/kinde-typescript-sdk'
+// @ts-expect-error virtual file
 import { version as frameworkVersion } from 'kinde-version.mjs'
 import { useRuntimeConfig } from '#imports'
 
-// @ts-expect-error virtual file
 
 let kindeClient: ACClient
 
