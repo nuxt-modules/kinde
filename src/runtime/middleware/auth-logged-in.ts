d280c593d061b16342b650dceaff120c8fde1eb0
Daniel Roe <daniel@roe.dev>
2024-04-11 08:15:37 +0200

    perf: simplify route middleware

--- src/runtime/middleware/auth-logged-in.ts
+++ src/runtime/middleware/auth-logged-in.ts
@@ -1,6 +1,4 @@
-import type { NitroRouteRules } from 'nitropack'
 import { getRouteRules } from 'nitropack/runtime'
-import { getKindeClient } from '../server/utils/client'
 import {
   abortNavigation,
   createError,
@@ -21,27 +19,24 @@ function rejectNavigation(statusCode: number, message: string) {
 
 export default defineNuxtRouteMiddleware(async () => {
   const nuxt = useNuxtApp()
-  const kindeConfig: NitroRouteRules['kinde'] = getRouteRules(nuxt.ssrContext!.event).kinde
+  const kindeConfig = getRouteRules(nuxt.ssrContext!.event).kinde
 
-  if (!nuxt.$auth.loggedIn) {
+  function denyAccess() {
     if (kindeConfig?.redirectUrl) {
       return navigateTo(kindeConfig.redirectUrl)
     }
     return rejectNavigation(401, 'You must be logged in to access this page')
   }
 
+  if (!nuxt.$auth.loggedIn) {
+    return denyAccess()
+  }
+
   if (kindeConfig?.permissions) {
-    const kinde = getKindeClient()
+    const usersPermissions = await nuxt.ssrContext!.event.context.kinde.getPermissions()
 
-    const accessPermissions = kindeConfig.permissions
-    const usersPermissions = await kinde.getPermissions(nuxt.ssrContext!.event.context.kinde.sessionManager!)
-    const hasCommonValue = accessPermissions?.some(item => usersPermissions.permissions.includes(item)) || false
-
-    if (!hasCommonValue) {
-      if (kindeConfig.redirectUrl) {
-        return navigateTo(kindeConfig.redirectUrl)
-      }
-      return rejectNavigation(401, 'You must be logged in to access this page')
+    if (!kindeConfig.permissions?.some(item => usersPermissions.permissions.includes(item))) {
+      return denyAccess()
     }
   }
 })
