76c14717469600ab15c1ce966a26e8bf956dcbcc
Daniel Rivers <me@danielrivers.com>
2024-08-19 22:13:01 +0100

    feat: add `postLoginRedirectURL` support (#137)

--- src/runtime/server/api/login.get.ts
+++ src/runtime/server/api/login.get.ts
@@ -5,7 +5,14 @@ import { useRuntimeConfig } from '#imports'
 export default defineEventHandler(async (event) => {
   const { kinde: kindeSettings } = useRuntimeConfig()
   const query: Record<string, string> = getQuery(event)
-  const loginURL = await getKindeClient().login(event.context.kinde.sessionManager, {
+
+  const sessionManager = event.context.kinde.sessionManager
+
+  if (query.postLoginRedirectURL) {
+    sessionManager.setSessionItem('post-login-redirect-url', query.postLoginRedirectURL)
+  }
+
+  const loginURL = await getKindeClient().login(sessionManager, {
     authUrlParams: {
       audience: kindeSettings.audience,
       ...query,
