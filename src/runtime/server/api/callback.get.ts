c0ed6390cf666cccda790502fefd38b48336684c
Daniel Rivers <me@danielrivers.com>
2024-08-19 20:38:30 +0100

    feat: add postLoginRedirectURL support

--- src/runtime/server/api/callback.get.ts
+++ src/runtime/server/api/callback.get.ts
@@ -5,9 +5,20 @@ import { useRuntimeConfig } from '#imports'
 const config = useRuntimeConfig()
 
 export default defineEventHandler(async (event) => {
+  const sessionManager = event.context.kinde.sessionManager
+
   await getKindeClient().handleRedirectToApp(
-    event.context.kinde!.sessionManager,
+    sessionManager,
     getRequestURL(event),
   )
+
+  const postLoginRedirectURL = await sessionManager.getSessionItem('post-login-redirect-url') as string
+
+  if (postLoginRedirectURL) {
+    await sessionManager.removeSessionItem('post-login-redirect-url')
+    await sendRedirect(event, postLoginRedirectURL)
+    return
+  }
+
   await sendRedirect(event, config.kinde.postLoginRedirectURL || '/')
 })
