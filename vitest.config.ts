88f771467212f87b301879df11fa1d1470b0abaf
Daniel Roe <daniel@roe.dev>
2025-09-17 11:11:55 +0100

    chore: tidy other unneeded files

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,12 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 export default defineVitestConfig({
   test: {
     environment: 'nuxt',
+    environmentOptions: {
+      nuxt: {
+        overrides: {
+          modules: ['@nuxtjs/kinde'],
+        },
+      },
+    },
   },
 })
